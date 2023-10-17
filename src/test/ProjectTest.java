03f597f77fa649d498152f31bf6450b9a3cc2033
Pedro Augusto <pedroagtrodrigues@gmail.com>
2023-10-17 13:55:34 -0300

    End Project

--- src/test/ProjectTest.java
+++ src/test/ProjectTest.java
@@ -31,10 +31,15 @@ public class ProjectTest {
         Customer customer2 = new Customer("Bianca", customer2Birth, "1199999-0002", "rm2@fiap.com.br", "FIAP - Paulista", "123.456.789-02");
         LocalDate customer3Birth = LocalDate.of(1997, Month.JANUARY, 14);
         Customer customer3 = new Customer("Rafael", customer3Birth, "1199999-0003", "rm3@fiap.com.br", "FIAP - Modulo", "123.456.789-03");
+        LocalDate customer4Birth = LocalDate.of(1992, Month.DECEMBER, 20);
+        Customer customer4 = new Customer("Armando", customer4Birth, "1199999-0004", "rm4@fiap.com.br", "FIAP - Vila Olimpia", "123.456.789-04");
+
 
         customersList.add(customer1);
         customersList.add(customer2);
         customersList.add(customer3);
+        customersList.add(customer4);
+
 
         for (Customer customer : customersList) {
             if (isDuplicateCPF(customer, customersSet)) {
@@ -44,7 +49,7 @@ public class ProjectTest {
                 customersMap.put(customer.getCpf(), customer);
             }
         }
-        String cpfToFind = "123.456.789-01";
+        String cpfToFind = "123.456.789-04";
         Customer foundCustomer = findCustomerByCPF(cpfToFind, customersList);
 
         if (foundCustomer != null) {
